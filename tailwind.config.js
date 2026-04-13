6d03c672588a1a4fa7dd4c082c7b002327239797
papelblume <jrodriguezj93@gmail.com>
2026-04-12 22:24:15 -0400

    Revise tailwind.config.js for Tailwind styles
    Updated Tailwind CSS configuration to include base, components, and utilities styles, along with custom styles for body and .wasd-button.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: system-ui, sans-serif;
+}
+
+.wasd-button {
+  @apply shadow-md font-medium px-3 w-20 py-1 my-1 cursor-pointer rounded-md hover:scale-105
 }
