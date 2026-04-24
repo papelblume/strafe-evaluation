9b541fa335a154016f3ef60ad64e064c8f73cbac
papelblume <jrodriguezj93@gmail.com>
2026-04-24 17:20:09 -0400

    Modify theme colors in tailwind.config.js
    Updated color values for theme customization.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#88a56f',
-        'accent': '#8cb5a8',
-        'secondary': '#a5c5ae',
-        'dark': '#3a3f36',
-        'bright': '#e8e8e8',
+        'primary': '#6db85c',
+        'accent': '#647858',
+        'secondary': '#647058',
+        'dark': '#4a5240',      // bg in dark mode / text in light mode
+        'bright': '#e8ead4',    // text in dark mode / bg in light mode
       },
     },
   },
